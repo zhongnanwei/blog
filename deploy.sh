b51cc6c05183df0e84bf32763a8ba0e2ea4b85b9
Nanwei <1939653635@qq.com>
2023-03-24 18:34:16 +0800

    deploy.sh 命令修改

--- deploy.sh
+++ deploy.sh
@@ -11,16 +11,16 @@ dist_path=docs/.vuepress/dist/ # 打包生成的文件夹路径
 push_branch=gh-pages # 推送的分支
 
 # 生成静态文件
-# npm run build
+npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist/
-git remote remove origin
+git init
 git remote add origin https://github.com/zhongnanwei/zhongnanwei.github.io.git
-
+git branch -m master main
 git add . 
 git commit -m "deploy, $commit_info"
-git push --set-upstream origin main --force
+git push --set-upstream origin main -f
 
 cd -
 rm -rf $dist_path
