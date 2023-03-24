dfcad98d15a8508a4bda424309f08f647f95bac2
Nanwei <1939653635@qq.com>
2023-03-24 18:14:09 +0800

    deploy, heads/master-0-g08e29ab

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,21 @@
 set -e
 
 
-# push_addr=`git remote get-url --push origin` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+# push_addr=`git remote get-url --push origin` # git提交地址，也可以手动设置，比如：
 push_addr=https://github.com/zhongnanwei/zhongnanwei.github.io.git
 commit_info=`git describe --all --always --long`
 dist_path=docs/.vuepress/dist/ # 打包生成的文件夹路径
 push_branch=gh-pages # 推送的分支
 
 # 生成静态文件
-npm run build
+# npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist/
-
 git remote remove origin
 git remote add origin https://github.com/zhongnanwei/zhongnanwei.github.io.git
 
-git add -A
+git add -A 
 git commit -m "deploy, $commit_info"
 git push --set-upstream origin main --force
 
